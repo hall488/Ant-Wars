3c44d0094befdb84a2f4ddc77d80224889803498
Ubuntu <christopherhall@christopherh008.hzterscemazurawp3xenxzahla.bx.internal.cloudapp.net>
2023-06-08 20:59:53 +0000

    test

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import logoImg from './assets/logo.png';
 
 class MyGame extends Phaser.Scene
 {
-    constructor ()
-    {
-        super();
-    }
+    slogo;    
 
     preload ()
     {
@@ -15,16 +12,12 @@ class MyGame extends Phaser.Scene
       
     create ()
     {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
+        this.slogo = this.add.image(400, 150, 'logo');
+    }
+
+    update () 
+    {
+        this.slogo.rotation += 0.01;
     }
 }
 
